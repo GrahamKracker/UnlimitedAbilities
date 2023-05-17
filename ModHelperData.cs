70d9318b30f43ab2f22af29cd84abce220392800
GrahamKracker <86011515+GrahamKracker@users.noreply.github.com>
2023-05-17 10:29:56 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,9 +3,9 @@
 public static class ModHelperData
 {
     public const string Name = "UnlimitedAbilities";
-    public const string Description = "UnlimitedAbilities by GrahamKracker";
+    public const string Description = "Disables the limit to the number of abilites in a roundr";
     public const string Version = "1.0.0";
     public const string RepoOwner = "GrahamKracker";
     public const string RepoName = "UnlimitedAbilities";
-    public const string WorksOnVersion = "35.0";
+    public const string WorksOnVersion = "36.3";
 }
